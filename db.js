6e7a8a4f7c714873306d480f58da294a6f6d13a1
thienth <thienth32@gmail.com>
2018-05-30 13:08:45 +0700

    update add item

---
+++ db.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyDyRJP1MTCQjiE8yjs001RkVrd2sl96kwM",
+	authDomain: "pt12311-web.firebaseapp.com",
+	databaseURL: "https://pt12311-web.firebaseio.com",
+	projectId: "pt12311-web",
+	storageBucket: "pt12311-web.appspot.com",
+	messagingSenderId: "876099975341"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
