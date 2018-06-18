61f860705c7f5bc69f7c88f7501ee2e70d83937b
thienth32 <thienth32@gmail.com>
2018-06-18 12:56:46 +0700

    update

---
+++ jsajax/data.php
@@ -0,0 +1,18 @@
+<?php 
+
+$arr = [
+	['title' => 'bai viet 1', 'content' => 'noi dung 1'],
+	['title' => 'bai viet 2', 'content' => 'noi dung 2'],
+	['title' => 'bai viet 3', 'content' => 'noi dung 3'],
+	['title' => 'bai viet 4', 'content' => 'noi dung 4'],
+	['title' => 'bai viet 5', 'content' => 'noi dung 5'],
+	['title' => 'bai viet 6', 'content' => 'noi dung 6'],
+	['title' => 'bai viet 7', 'content' => 'noi dung 7'],
+];
+
+sleep(3);
+
+echo json_encode($arr);
+
+ ?>
+
